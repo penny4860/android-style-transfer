ae20a39148707642f3ff75014e0dcdb95407eccb
jeongjoonsup <penny4860@gmail.com>
2019-01-19 16:45:15 +0900

    init : create android project

---
+++ app/src/main/java/compenny4860/github/tensorflowliteexample/MainActivity.java
@@ -0,0 +1,13 @@
+package compenny4860.github.tensorflowliteexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
