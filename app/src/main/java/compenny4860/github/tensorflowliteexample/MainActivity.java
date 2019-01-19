67729410e5b569d9a98ef367d6367c7edbe7d2d5
jeongjoonsup <penny4860@gmail.com>
2019-01-19 17:14:51 +0900

    main activity

--- app/src/main/java/compenny4860/github/tensorflowliteexample/MainActivity.java
+++ app/src/main/java/compenny4860/github/tensorflowliteexample/MainActivity.java
@@ -1,13 +1,24 @@
 package compenny4860.github.tensorflowliteexample;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+/** Main {@code Activity} class for the Camera app. */
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (null == savedInstanceState) {
+
+            // 런타임에 액티비티에 프래그먼트 추가하는 과정 : 특정 Resource에 프래그먼트를 추가
+            // R.id.container : 리소스파일에서 정의
+            getFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.container, ImageFragment.newInstance())
+                    .commit();
+
+        }
     }
 }
