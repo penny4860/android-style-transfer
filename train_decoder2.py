6a1eeb7f2999948414d33392fa6d25b2982da937
jeongjoonsup <penny4860@gmail.com>
2019-02-14 22:43:33 +0900

    nit

--- train_decoder2.py
+++ train_decoder2.py
@@ -124,14 +124,6 @@ if __name__ == '__main__':
                                             encoder_model=vgg_encoder_model,
                                             combine_decoder_model=vgg_combine_decoder,
                                             input_size=input_size)
-#     import matplotlib.pyplot as plt
-#     for j in range(5):
-#         xs, ys = train_generator[j]
-#         for i in range(4):
-#             plt.imshow(ys[i].astype(np.uint8))
-#             plt.show()
-      
-#     # valid_generator = BatchGenerator(fnames[160:], batch_size=4, shuffle=False)
        
     # 2. create loss function
     if USE_TF_KERAS:
