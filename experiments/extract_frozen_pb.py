88d5086af685f553a848b9888b68fe7a76c62be6
jeongjoonsup <penny4860@gmail.com>
2019-02-17 01:42:49 +0900

    build mobile decoder pb

--- experiments/extract_frozen_pb.py
+++ experiments/extract_frozen_pb.py
@@ -1,19 +1,20 @@
 
 import tensorflow as tf
 from adain.graph import freeze_session
+from adain import PKG_ROOT
 
 if __name__ == '__main__':
-    from adain.encoder import vgg19_light
+    from adain.transfer_decoder import build_mobile_combine_decoder
     # encoder_model = vgg_encoder()
-    model = vgg19_light()
-    model.load_weights("../mobile_encoder.h5", by_name=False)
+    model = build_mobile_combine_decoder()
+    model.load_weights(PKG_ROOT + "/models/h5/mobile_decoder.h5")
     model.summary()
         
     # 1. to frozen pb
     K = tf.keras.backend
     frozen_graph = freeze_session(K.get_session(),
                                   output_names=[out.op.name for out in model.outputs])
-    tf.train.write_graph(frozen_graph, "models", "encoder.pb", as_text=False)
+    tf.train.write_graph(frozen_graph, "models", "decoder.pb", as_text=False)
     # input_c,input_s  / output/mul
     for t in model.inputs + model.outputs:
         print("op name: {}, shape: {}".format(t.op.name, t.shape))
