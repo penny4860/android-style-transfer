ade3f4d31dbde196e03a73664fd93f40ed9f3ba7
jeongjoonsup <penny4860@gmail.com>
2019-02-17 21:15:29 +0900

    decoder optimized pb 파일 빌드 성공

--- experiments/extract_frozen_pb.py
+++ experiments/extract_frozen_pb.py
@@ -8,33 +8,35 @@ from adain import PKG_ROOT
 if __name__ == '__main__':
     from adain.encoder import vgg_encoder, mobile_encoder
     from adain.transfer_decoder import build_mobile_combine_decoder
+    tf.keras.backend.set_learning_phase(0) # this line most important
 
     ##########################################################################
-    pb_fname = "mobile_decoder.pb"
+    pb_fname = "decoder.pb"
+    output_pb_fname = "decoder_opt.pb"
+    input_node = "input_c,input_s"
+    output_node = "b1_layer1_conv3x3/Relu"
     ##########################################################################
 
-#     model = build_mobile_combine_decoder()
-#     model.load_weights(PKG_ROOT + "/models/h5/mobile_decoder.h5")
-#     model.summary()
-#         
-#     # 1. to frozen pb
-#     K = tf.keras.backend
-#     frozen_graph = freeze_session(K.get_session(),
-#                                   output_names=[out.op.name for out in model.outputs])
-#     tf.train.write_graph(frozen_graph, "models", pb_fname, as_text=False)
-#     # input_c,input_s  / output/mul
-#     for t in model.inputs + model.outputs:
-#         print("op name: {}, shape: {}".format(t.op.name, t.shape))
+    model = build_mobile_combine_decoder()
+    model.load_weights(PKG_ROOT + "/models/h5/mobile_decoder.h5")
+    model.summary()
+        
+    # 1. to frozen pb
+    K = tf.keras.backend
+    frozen_graph = freeze_session(K.get_session(),
+                                  output_names=[out.op.name for out in model.outputs])
+    tf.train.write_graph(frozen_graph, "models", pb_fname, as_text=False)
+    # input_c,input_s  / output/mul
+    for t in model.inputs + model.outputs:
+        print("op name: {}, shape: {}".format(t.op.name, t.shape))
 
-    # graph optimize : tf 1.4.0
-#     cmd = 'python -m tensorflow.python.tools.optimize_for_inference \
-#             --input models/{} \
-#             --output {} \
-#             --input_names={} \
-#             --output_names={}'.format(pb_fname, output_pb_fname, input_node, output_node)
-#     subprocess.call(cmd, shell=True)
-# 
-    sess = load_graph_from_pb("models/opt.pb")
+    cmd = 'python -m tensorflow.python.tools.optimize_for_inference \
+            --input models/{} \
+            --output {} \
+            --input_names={} \
+            --output_names={}'.format(pb_fname, output_pb_fname, input_node, output_node)
+    subprocess.call(cmd, shell=True)
+
+    sess = load_graph_from_pb(output_pb_fname)
     print(sess)
-
 
