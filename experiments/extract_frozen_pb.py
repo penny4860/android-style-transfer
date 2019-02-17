f5eaecc38afb0e2d30cfad0dc2951256fc2e56ca
jeongjoonsup <penny4860@gmail.com>
2019-02-17 19:20:35 +0900

    graph optimize 코드를 python 에서 실행

--- experiments/extract_frozen_pb.py
+++ experiments/extract_frozen_pb.py
@@ -1,23 +1,33 @@
 
 import tensorflow as tf
+import subprocess
 from adain.graph import freeze_session
 from adain import PKG_ROOT
 
 if __name__ == '__main__':
+    from adain.encoder import vgg_encoder, mobile_encoder
     from adain.transfer_decoder import build_mobile_combine_decoder
-    # encoder_model = vgg_encoder()
-    model = build_mobile_combine_decoder()
-    model.load_weights(PKG_ROOT + "/models/h5/mobile_decoder.h5")
+
+    pb_fname = "encoder.pb"
+
+    model = mobile_encoder()
+    model.load_weights(PKG_ROOT + "/models/h5/mobile_encoder.h5")
     model.summary()
         
     # 1. to frozen pb
     K = tf.keras.backend
     frozen_graph = freeze_session(K.get_session(),
                                   output_names=[out.op.name for out in model.outputs])
-    tf.train.write_graph(frozen_graph, "models", "decoder.pb", as_text=False)
+    tf.train.write_graph(frozen_graph, "models", pb_fname, as_text=False)
     # input_c,input_s  / output/mul
     for t in model.inputs + model.outputs:
         print("op name: {}, shape: {}".format(t.op.name, t.shape))
 
-    # python -m tensorflow.python.tools.optimize_for_inference --input encoder.pb --output mobile_encoder_opt.pb --input_names=input --output_names=output/Relu
+    cmd = 'python -m tensorflow.python.tools.optimize_for_inference \
+            --input {} \
+            --output {} \
+            --input_names={} \
+            --output_names={}'.format("models/encoder.pb", "mobile_encoder_opt.pb", "input", "output/Relu")
+    subprocess.call(cmd, shell=True)
+
 
