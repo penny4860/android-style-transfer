2d4f8bd9734d6be237d4db2e07b9ffc314d62dd5
jeongjoonsup <penny4860@gmail.com>
2019-02-23 00:46:44 +0900

    nit

--- recon.py
+++ recon.py
@@ -40,6 +40,7 @@ if __name__ == '__main__':
     # 3. encoding
     encoder = mobile_encoder(input_size=encoder_input)
     mobile_decoder = build_mobile_combine_decoder(decoder_input, DEFAULT_NEW_BLOCK)
+    # mobile_decoder.load_weights("adain/models/h5/mobile_decoder.h5", by_name=True)
     mobile_decoder.load_weights("mobile_decoder.h5", by_name=True)
 
     c_features = encoder.predict(c_img_prep)
