65f32b0007b7f02ae3d955aa10c8b9a90942bd2a
jeongjoonsup <penny4860@gmail.com>
2019-03-26 23:12:39 +0900

    USE_TF_KERAS 옵션 추가

--- linear/encoder.py
+++ linear/encoder.py
@@ -1,23 +1,35 @@
 # -*- coding: utf-8 -*-
 
 import tensorflow as tf
+import keras
 import os
 from adain import PROJECT_ROOT
 from adain.layers import VggPreprocess
+from linear import USE_TF_KERAS
 
-# if USE_TF_KERAS:
-Input = tf.keras.layers.Input
-Conv2D = tf.keras.layers.Conv2D
-BatchNormalization = tf.keras.layers.BatchNormalization
-Activateion = tf.keras.layers.Activation
-MaxPooling2D = tf.keras.layers.MaxPooling2D
-Layer = tf.keras.layers.Layer
-Model = tf.keras.models.Model
-VGG19 = tf.keras.applications.vgg19.VGG19
-
-DepthwiseConv2D = tf.keras.layers.DepthwiseConv2D
-Activation = tf.keras.layers.Activation
-
+if USE_TF_KERAS:
+    Input = tf.keras.layers.Input
+    Conv2D = tf.keras.layers.Conv2D
+    BatchNormalization = tf.keras.layers.BatchNormalization
+    Activateion = tf.keras.layers.Activation
+    MaxPooling2D = tf.keras.layers.MaxPooling2D
+    Layer = tf.keras.layers.Layer
+    Model = tf.keras.models.Model
+    VGG19 = tf.keras.applications.vgg19.VGG19
+    DepthwiseConv2D = tf.keras.layers.DepthwiseConv2D
+    Activation = tf.keras.layers.Activation
+else:
+    Input = keras.layers.Input
+    Conv2D = keras.layers.Conv2D
+    BatchNormalization = keras.layers.BatchNormalization
+    Activateion = keras.layers.Activation
+    MaxPooling2D = keras.layers.MaxPooling2D
+    Layer = keras.layers.Layer
+    Model = keras.models.Model
+    VGG19 = keras.applications.vgg19.VGG19
+    DepthwiseConv2D = keras.layers.DepthwiseConv2D
+    Activation = keras.layers.Activation
+    
 
 def mobile_encoder(input_size=256, fname=None):
     def _build_model(input_shape):
@@ -28,43 +40,71 @@ def mobile_encoder(input_size=256, fname=None):
 
         # Block 1
         x = Conv2D(32, (3, 3), strides=2, use_bias=False, padding='same')(x)
-        x = BatchNormalization(fused=False)(x)
+        
+        if USE_TF_KERAS:
+            x = BatchNormalization(fused=False)(x)
+        else:
+            x = BatchNormalization()(x)
         x = Activation("relu")(x)
         
         # x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv2')(x)
         x = DepthwiseConv2D((3, 3), use_bias=False, padding='same')(x)
-        x = BatchNormalization(fused=False)(x)
+        if USE_TF_KERAS:
+            x = BatchNormalization(fused=False)(x)
+        else:
+            x = BatchNormalization()(x)
         x = Activation("relu")(x)
         x = Conv2D(64, (1, 1), use_bias=False, padding='same')(x)
-        x = BatchNormalization(fused=False)(x)
+        if USE_TF_KERAS:
+            x = BatchNormalization(fused=False)(x)
+        else:
+            x = BatchNormalization()(x)
         x = Activation("relu")(x)
 #         x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
            
         # Block 2
         # x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv1')(x)
         x = DepthwiseConv2D((3, 3), strides=2, use_bias=False, padding='same')(x)
-        x = BatchNormalization(fused=False)(x)
+        if USE_TF_KERAS:
+            x = BatchNormalization(fused=False)(x)
+        else:
+            x = BatchNormalization()(x)
         x = Activation("relu")(x)
         x = Conv2D(128, (1, 1), use_bias=False, padding='same')(x)
-        x = BatchNormalization(fused=False)(x)
+        if USE_TF_KERAS:
+            x = BatchNormalization(fused=False)(x)
+        else:
+            x = BatchNormalization()(x)
         x = Activation("relu")(x)
 
         # x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv2')(x)
         x = DepthwiseConv2D((3, 3), use_bias=False, padding='same')(x)
-        x = BatchNormalization(fused=False)(x)
+        if USE_TF_KERAS:
+            x = BatchNormalization(fused=False)(x)
+        else:
+            x = BatchNormalization()(x)
         x = Activation("relu")(x)
         x = Conv2D(128, (1, 1), use_bias=False, padding='same')(x)
-        x = BatchNormalization(fused=False)(x)
+        if USE_TF_KERAS:
+            x = BatchNormalization(fused=False)(x)
+        else:
+            x = BatchNormalization()(x)
         x = Activation("relu")(x)
 #         x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
            
         # Block 3
         # x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1')(x)
         x = DepthwiseConv2D((3, 3), use_bias=False, padding='same')(x)
-        x = BatchNormalization(fused=False)(x)
+        if USE_TF_KERAS:
+            x = BatchNormalization(fused=False)(x)
+        else:
+            x = BatchNormalization()(x)
         x = Activation("relu")(x)
         x = Conv2D(256, (1, 1), use_bias=False, padding='same')(x)
-        x = BatchNormalization(fused=False)(x)
+        if USE_TF_KERAS:
+            x = BatchNormalization(fused=False)(x)
+        else:
+            x = BatchNormalization()(x)
         x = Activation("relu", name="block3_conv1")(x)
 
         model = Model(img_input, x, name='mobile_encoder_31')
@@ -124,24 +164,24 @@ if __name__ == '__main__':
     model = mobile_encoder(input_size=256)
     model.summary()
          
-    # 1. to frozen pb
-    K = tf.keras.backend
-    frozen_graph = freeze_session(K.get_session(),
-                                  output_names=[out.op.name for out in model.outputs])
-    tf.train.write_graph(frozen_graph, "models", pb_fname, as_text=False)
-    # input_c,input_s  / output/mul
-    for t in model.inputs + model.outputs:
-        print("op name: {}, shape: {}".format(t.op.name, t.shape))
-   
-    cmd = 'python -m tensorflow.python.tools.optimize_for_inference \
-            --input models/{} \
-            --output {} \
-            --input_names={} \
-            --output_names={}'.format(pb_fname, output_pb_fname, input_node, output_node)
-    subprocess.call(cmd, shell=True)
-    
-    sess = load_graph_from_pb(output_pb_fname)
-    print(sess)
+#     # 1. to frozen pb
+#     K = tf.keras.backend
+#     frozen_graph = freeze_session(K.get_session(),
+#                                   output_names=[out.op.name for out in model.outputs])
+#     tf.train.write_graph(frozen_graph, "models", pb_fname, as_text=False)
+#     # input_c,input_s  / output/mul
+#     for t in model.inputs + model.outputs:
+#         print("op name: {}, shape: {}".format(t.op.name, t.shape))
+#    
+#     cmd = 'python -m tensorflow.python.tools.optimize_for_inference \
+#             --input models/{} \
+#             --output {} \
+#             --input_names={} \
+#             --output_names={}'.format(pb_fname, output_pb_fname, input_node, output_node)
+#     subprocess.call(cmd, shell=True)
+#     
+#     sess = load_graph_from_pb(output_pb_fname)
+#     print(sess)
 
 
 #     graph_def_file = "models/" + pb_fname
