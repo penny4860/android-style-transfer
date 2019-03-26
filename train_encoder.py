69053a57dea5bdd62d0a520f38df0d168f0be1f6
jeongjoonsup <penny4860@gmail.com>
2019-03-26 22:58:55 +0900

    train_encoder 드라이버

--- train_encoder.py
+++ train_encoder.py
@@ -7,9 +7,8 @@ import glob
 import keras
 
 np.random.seed(1337)
-from adain.encoder import vgg19_light, vgg19
 from adain.generator import BatchGenerator, create_callbacks
-
+from linear.encoder import vgg_encoder, mobile_encoder
 
 DEFAULT_IMG_ROOT = os.path.join("experiments", "imgs")
 DEFAULT_BATCH_SIZE = 8
@@ -44,28 +43,24 @@ if __name__ == '__main__':
     args = argparser.parse_args()
     
     input_size = 256
-    model = vgg19_light(input_shape=[input_size,input_size,3])
-    model.load_weights("mobile_encoder.h5", by_name=True)
-    truth_encoder = vgg19(t7_file=None, input_shape=[input_size,input_size,3])
-    truth_encoder.load_weights(DEFAULT_VGG_WEIGHTS)
+    teacher_model = vgg_encoder(input_size)
+    student_model = mobile_encoder(input_size)
     
-    fnames = glob.glob(args.image_root + "/*.jpg")
+    fnames = glob.glob(args.image_root + "/**/*.jpg")
     print("{}-files to train".format(len(fnames)))
     train_generator = BatchGenerator(fnames,
                                      batch_size=min(args.batch_size, len(fnames)),
-                                     truth_model=truth_encoder,
-                                     shuffle=False)
+                                     shuffle=False,
+                                     truth_model=teacher_model,
+                                     input_size=input_size)
     # valid_generator = BatchGenerator(fnames[160:], batch_size=4, shuffle=False)
     
     # 2. create loss function
-    model.compile(loss="mean_squared_error",
+    student_model.compile(loss="mean_squared_error",
                   optimizer=keras.optimizers.Adam(lr=args.learning_rate))
-    model.fit_generator(train_generator,
-                        steps_per_epoch=len(train_generator),
-                        callbacks=create_callbacks(),
-                        validation_data  = train_generator,
-                        validation_steps = len(train_generator),
-                        epochs=1000)
-
-
-
+    student_model.fit_generator(train_generator,
+                                steps_per_epoch=len(train_generator),
+                                callbacks=create_callbacks(),
+                                validation_data  = train_generator,
+                                validation_steps = len(train_generator),
+                                epochs=1000)
