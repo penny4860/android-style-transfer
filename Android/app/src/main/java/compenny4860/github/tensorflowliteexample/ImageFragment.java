8af4cae6f555ffa8ae43978b880499f8c0ebf4d2
jeongjoonsup <penny4860@gmail.com>
2019-04-01 21:31:31 +0900

    테스트 배너광고 로드 성공

--- Android/app/src/main/java/compenny4860/github/tensorflowliteexample/ImageFragment.java
+++ Android/app/src/main/java/compenny4860/github/tensorflowliteexample/ImageFragment.java
@@ -18,6 +18,7 @@ import android.widget.ImageView;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 
 import java.io.IOException;
 import java.util.LinkedList;
@@ -60,6 +61,7 @@ public class ImageFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_image, container, false);
         styleImageView = v.findViewById(R.id.styleImageView);
 
+        MobileAds.initialize(getActivity(), "ca-app-pub-3940256099942544~3347511713");
         mAdView = v.findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
