b44aa5edd17fe773cdaaecd3bbf052f49fae364e
jeongjoonsup <penny4860@gmail.com>
2019-08-25 16:10:16 +0900

    code 정리

--- experiments/tflite_adain.py
+++ experiments/tflite_adain.py
@@ -3,21 +3,20 @@
 import numpy as np
 import cv2
 import os
+import tensorflow as tf
 
 from adain.utils import preprocess, plot
-from adain import MODEL_ROOT, PROJECT_ROOT
+from adain import MODEL_ROOT
 
 DEFAULT_ENCODER_H5 = os.path.join(MODEL_ROOT, "h5", "vgg_encoder.h5")
 DEFAULT_DECODER_H5 = os.path.join(MODEL_ROOT, "h5", "vgg_decoder.h5")
 
 content_fname="../input/content/chicago.jpg"
 style_fname="../input/style/asheville.jpg"
-alpha = 1.0
 img_size = 416
 
 
-def run_tflite(model, input_data):
-    import tensorflow as tf
+def extract_feat_tflite(model, input_data):
     # Convert to TF Lite format
     converter = tf.lite.TFLiteConverter.from_keras_model(model)
     tflite_model = converter.convert()
@@ -40,33 +39,9 @@ def run_tflite(model, input_data):
     return output_data
 
 
-if __name__ == '__main__':
-    
-    # 1. contents / style images
-    c_img = cv2.imread(content_fname)[:,:,::-1]
-    s_img = cv2.imread(style_fname)[:,:,::-1]
-
-    # 2. load input imgs
-    c_img_prep = preprocess(c_img, (img_size,img_size))
-    s_img_prep = preprocess(s_img, (img_size,img_size))
-    
-    # 3. encoding
-    from adain.encoder import vgg_encoder
-    model = vgg_encoder(img_size)
-    model.load_weights(DEFAULT_ENCODER_H5)
-    
-    c_features = run_tflite(model, c_img_prep)
-    s_features = run_tflite(model, s_img_prep)
-    print(c_features.shape, s_features.shape)
-    
- 
-    from adain.decoder import combine_and_decode_model
-    decoder = combine_and_decode_model(img_size/8)
-    decoder.load_weights(DEFAULT_DECODER_H5)
-     
-    import tensorflow as tf
+def decode(model, s_features, c_features):
     # Convert to TF Lite format
-    converter = tf.lite.TFLiteConverter.from_keras_model(decoder)
+    converter = tf.lite.TFLiteConverter.from_keras_model(model)
     tflite_model = converter.convert()
      
     # Load TFLite model and allocate tensors.
@@ -89,6 +64,34 @@ if __name__ == '__main__':
 
     output_details = interpreter.get_output_details()
     stylized_imgs = interpreter.get_tensor(output_details[0]['index'])
+    return stylized_imgs
+
+
+if __name__ == '__main__':
+    
+    # 1. contents / style images
+    c_img = cv2.imread(content_fname)[:,:,::-1]
+    s_img = cv2.imread(style_fname)[:,:,::-1]
+
+    # 2. load input imgs
+    c_img_prep = preprocess(c_img, (img_size,img_size))
+    s_img_prep = preprocess(s_img, (img_size,img_size))
+    
+    # 3. encoding
+    from adain.encoder import vgg_encoder
+    model = vgg_encoder(img_size)
+    model.load_weights(DEFAULT_ENCODER_H5)
+    
+    c_features = extract_feat_tflite(model, c_img_prep)
+    s_features = extract_feat_tflite(model, s_img_prep)
+    print(c_features.shape, s_features.shape)
+    
+ 
+    from adain.decoder import combine_and_decode_model
+    decoder = combine_and_decode_model(img_size/8)
+    decoder.load_weights(DEFAULT_DECODER_H5)
+    stylized_imgs = decode(decoder, s_features, c_features)
+     
     stylized_img = stylized_imgs[0].astype(np.uint8)
     
     # 5. plot
